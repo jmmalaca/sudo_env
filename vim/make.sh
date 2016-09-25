48ee95d89a5e1bec8b08d39a816d8d49a10f7e96
José Miguel Malaca <jose.malaca@rupeal.com>
2016-09-25 20:33:45 +0100

    vim configuration and plugins

---
+++ vim/make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install Vim configurations
+
+echo "Install Vim confs"
+if [ -d "~/.vim" ]; then
+
+  echo "Copy Vim folders"
+  cp -r vim/*  ~/.vim/
+
+  echo "Install Vim config"
+  cp vimrc ~/.vimrc
+
+  echo "All good, write 'vim' to start,"
+  echo "First of all, to install vim pluggins do on vim:"
+  echo ":PluginInstall"
+  echo "Enjoy :D"
+else
+  echo "Error: Vim folder not found."
+fi
