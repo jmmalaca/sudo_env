e2f17c8fcdb1a0517505b26740f119e4c3e2ff88
José Miguel Malaca <jose.malaca@rupeal.com>
2016-10-15 12:24:38 +0100

    improve dots make.sh and remove osx file from list of dots files

--- dots/make.sh
+++ dots/make.sh
@@ -3,4 +3,25 @@
 
 echo "Copy Dots files"
 
-cp -r .* ~/
+#we could do: 'cp -r .* ~/', but lets set here the list:
+cp .aliases ~/.aliases  #about bash commands alias
+cp .bash_profile ~/.bash_profile   #bash configs, here is set the git-completion.bash
+cp .bash_prompt ~/.bash_prompt  #bash promp based on Solarized Dark theme
+cp .bashrc ~/.bashrc  #points to the bash_profile
+cp .curlrc ~/.curlrc  #about curls
+cp .exports ~/.exports  #eport command to set bash defaults
+cp .functions ~/.functions  #bash functions, i.e. 'mkd' will create a new dir and go to it
+cp .git-commit-template ~/.git-commit-template  #set git commits template
+cp .gitconfig ~/.gitconfig  #git configs
+cp .gitignore ~/.gitignore  #stuff to git ignore
+cp .gitmodules ~/.gitmodules  #git modules
+cp .gvimrc ~/.gvimrc  #vim configs
+cp .inputrc ~/.inputrc  #bash input config
+cp .rdebugrc ~/.rdebugrc  #needed for ruby on rails debug
+cp .wgetrc ~/.wgetrc  #wget config
+
+echo "copy git-completion"
+cp git-completion.bash ~/git-completion.bash
+
+echo "Setup macOS configs"
+./.osx
