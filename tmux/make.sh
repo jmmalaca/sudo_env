b129ab3ed70a9b5aea7ef46cf4b7d0a16dfeff3a
José Miguel Malaca <jose.malaca@rupeal.com>
2016-10-15 10:21:23 +0100

    update osx file having some personalized suff and improved tmux make file

--- tmux/make.sh
+++ tmux/make.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 # Build Tmux script
 
-echo "Clone Tmux"
-git clone https://github.com/tOOnPT/tmux.git
+if ! [[ $(ls -a ~/.tmux) ]]; then
+	echo "Clone Tmux"
+	git clone https://github.com/tOOnPT/tmux.git
 
-if [ -d "tmux" ]; then
+	if [ -d "tmux" ]; then
+		echo "Install Tmux"
+		cd tmux
+		sh autogen.sh
+		./configure && make
+		cd ..
+	else
+		echo "Clone tmux failed"
+	fi
+fi
 
-  echo "Install Tmux"
-  cd tmux
-  sh autogen.sh
-  ./configure && make
-  cd ..
+if [[ $(ls -a ~/.tmux.conf) ]]; then
+	echo "Install Tmux-config"
+	cp tmux.conf ~/.tmux.conf
 
-  echo "Install Tmux-config"
-  cp tmux.conf ~/.tmux.conf
+	echo "Reload config"
+	tmux source-file ~/.tmux.conf
 
-  echo "Reload config"
-  tmux source-file ~/.tmux.conf
+	echo "All good, write 'tmux' to start,"
+	echo "Your tmux prefix-command is now: 'CTRL + a'"
+	echo "First of all do: prefix + ( CTRL + I ) to install tmux pluggins"
+	echo "Enjoy :D"
 
-  echo "All good, write 'tmux' to start,"
-  echo "Your tmux prefix-command is now: 'CTRL + a'"
-  echo "First of all do: prefix + ( CTRL + I ) to install tmux pluggins"
-  echo "Enjoy :D"
-
-  echo "Tmux cheatsheet here: http://goo.gl/xLBhpw"
+	echo "Tmux cheatsheet here: http://goo.gl/xLBhpw"
 else
-  echo "Error: Tmux folder not found."
+	echo "Tmux config file doesn't exists."
 fi
