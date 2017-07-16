f60d2e9ecd89e51ad6ff428f14e08a40383df38e
José Miguel Malaca <jose.miguel.malaca@me.com>
2017-07-16 13:28:20 +0100

    improve tmux instalation

--- tmux/make.sh
+++ tmux/make.sh
@@ -36,6 +36,9 @@ if [[ $(ls -a ~/.tmux.conf) ]]; then
 	echo "Reload config"
 	tmux source-file ~/.tmux.conf
 
+	echo "Install ~/.tmux/plugins/tmp"
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 	echo "All good, write 'tmux' to start,"
 	echo "Your tmux prefix-command is now: 'CTRL + a'"
 	echo "First of all do: prefix + ( CTRL + I ) to install tmux pluggins"
